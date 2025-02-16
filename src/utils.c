fcf5950f60dd3bbf8b468058b2c88020155837c8
larocqueg <portugalgabriel2021@gmail.com>
2025-02-16 15:58:16 +0000

    did ft_child and ft_parent on utils.c

--- src/utils.c
+++ src/utils.c
@@ -12,12 +12,36 @@
 
 #include "../includes/pipex.h"
 
-void	child(char **av, char **env, int fd)
+void	child(char **av, char **env, int *fd)
 {
+	int	fd_infile;
+
+	fd_infile = open(av[1], O_RDONLY);
+	if (fd_infile == -1)
+		error();
+	else
+	{
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		dup2(fd_infile, STDOUT_FILENO);
+		execute(av[2], env);
+	}
 }
 
-void	parent(char **av, char **env, int fd)
+void	parent(char **av, char **env, int *fd)
 {
+	int	fd_outfile;
+
+	fd_outfile = open(av[4], O_WRONLY | O_CREAT | O_TRUNC);
+	if (fd_outfile == -1)
+		error ();
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDOUT_FILENO);
+		dup2(fd_outfile, STDOUT_FILENO);
+		execute(av[3], env);
+	}
 }
 
 void	ft_error(void)
